35812132ede6e629a398a56f42e43f9d042b6851
Suriya <true.many.032@gmail.com>
2025-02-26 15:28:07 +0700

    Assignment 1

--- ex-1.js
+++ ex-1.js
@@ -1,19 +1,20 @@
 // Exercise #1: Strict and Same
 // Start commenting here
-Object.is(10, 20);
-Object.is(0, -0);
-Object.is(NaN, 100);
-Object.is("one", "two");
-Object.is({ name: "John", age: 30 }, { name: "John", age: 30 });
-Object.is(null, undefined);
+Object.is(10, 20); //false
+Object.is(0, -0); //false
+Object.is(NaN, 100); //false
+Object.is("one", "two"); //false
+Object.is({ name: "John", age: 30 }, { name: "John", age: 30 }); //false
+Object.is(null, undefined); //false
 
-10 === 20;
-10 === "10"
-3.14 !== 3.14;
-"TechUp" !== "TechUp";
-NaN === NaN;
-0 !== -0;
+10 === 20; //false
+10 === "10" //false
+3.14 !== 3.14; //false
+"TechUp" !== "TechUp"; //false
+NaN === NaN; //false
+0 !== -0; //false
 
-let objectA = { course: "Web Developer", price: 300000 };
-let objectB = { course: "Web Developer", price: 300000 };
-objectA === objectB;
+let objectA = { course: "Web Developer", price: 300000 }; //false
+let objectB = { course: "Web Developer", price: 300000 }; //false
+objectA === objectB; //false
+
